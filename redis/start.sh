113cf681651200c7eed1f60fd61cb9afd3feeb0c
yesheng <yesheng@huya.com>
2021-06-15 21:42:55 +0800

    test

--- redis/start.sh
+++
@@ -1,4 +0,0 @@
-basepath=/Users/yesheng/Documents/docker/redis
-docker run -d -p 6379:6379 --name redis3.2  -v $basepath/redis.conf:/etc/redis/redis.conf -v $basepath/data:/data   redis:3.2 redis-server /etc/redis/redis.conf  --appendonly yes 
-
-
